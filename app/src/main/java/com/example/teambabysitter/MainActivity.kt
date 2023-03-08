85c5712ddc2d9444565e6563b275767c2ac42d10
dongu <1141501@naver.com>
2023-03-08 15:10:28 +0900

    help

--- app/src/main/java/com/example/teambabysitter/MainActivity.kt
+++ app/src/main/java/com/example/teambabysitter/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //나 좀 살려주세요!!
     }
 }
