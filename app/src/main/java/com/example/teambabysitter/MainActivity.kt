37316999824faa544524c9f005bef0260770c434
Minkyung Hwang <starminkeung@naver.com>
2023-03-14 22:28:42 +0900

    tool bar

--- app/src/main/java/com/example/teambabysitter/MainActivity.kt
+++ app/src/main/java/com/example/teambabysitter/MainActivity.kt
@@ -2,11 +2,51 @@ package com.example.teambabysitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.teambabysitter.navigation.DetailViewFragment
+import com.example.teambabysitter.navigation.GridFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_map -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_community -> {
+
+                return true
+            }
+            R.id.action_diary -> {
+                var alarmFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_mypage -> {
+                var userFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment).commit()
+            }
+        }
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
 
     }
 }
