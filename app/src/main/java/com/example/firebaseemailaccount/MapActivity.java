ab9aa3a38c17f80ae980710d4eb95c5c8497e142
Minkyung Hwang <starminkeung@naver.com>
2023-04-09 01:23:12 +0900

    map

--- app/src/main/java/com/example/firebaseemailaccount/MapActivity.java
+++ app/src/main/java/com/example/firebaseemailaccount/MapActivity.java
@@ -10,16 +10,105 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MapActivity extends Fragment {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapSdk;
+import com.naver.maps.map.OnMapReadyCallback;
+
+public class MapActivity extends Fragment implements OnMapReadyCallback {
     private View view;
     private String TAG = "MapFragment";
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater , @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        Log.i(TAG,"onCreateView");
+    //지도 객체 변수
+    private MapView mapView;
 
-        view=inflater.inflate(R.layout.activity_login,container,false);
-        return view;
+    public MapActivity() { }
+
+    public static MapActivity newInstance() {
+        MapActivity fragment = new MapActivity();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_map, container, false);
+
+        mapView = (MapView) rootView.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        NaverMapSdk.getInstance(getActivity()).setClient(
+                new NaverMapSdk.NaverCloudPlatformClient("najhW4IFjT6rXR0RSRMY" + "." + "x5al5NBWdoPFdJZy5EjxHi3vogria8mFemwolbZA")
+        );
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        //배경 지도 선택
+        naverMap.setMapType(NaverMap.MapType.Satellite);
+
+        //건물 표시
+        naverMap.setLayerGroupEnabled(naverMap.LAYER_GROUP_BUILDING, true);
+
+        //위치 및 각도 조정
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(33.38, 126.55),   // 위치 지정
+                9,                                     // 줌 레벨
+                45,                                       // 기울임 각도
+                45                                     // 방향
+        );
+        naverMap.setCameraPosition(cameraPosition);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
--- app/src/main/java/com/example/firebaseemailaccount/PageActivity.java
+++ app/src/main/java/com/example/firebaseemailaccount/PageActivity.java
@@ -15,11 +15,11 @@ public class PageActivity extends AppCompatActivity
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
-    //화면에 보여짐
-    setContentView(R.layout.bottom_navigation_container);
-    buttonview = findViewById(R.id.bottomNav);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //화면에 보여짐
+        setContentView(R.layout.bottom_navigation_container);
+        buttonview = findViewById(R.id.bottomNav);
         getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeActivity()).commit();
 
 // BottomNavigationView 선택 이벤트 처리
@@ -43,6 +43,6 @@ public class PageActivity extends AppCompatActivity
             }
             return true;
         });
-}
+    }
 }
 
