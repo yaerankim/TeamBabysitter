b77fd4e57193d75cbc413518156fe4d32591f3c5
Yaeran Kim <yaerankim000812@gmail.com>
2023-05-25 00:09:23 +0900

    로그아웃 시 cookie 삭제 및 리디렉션 코드 추가.

--- app/src/main/java/com/example/firebaseemailaccount/LoginActivity.java
+++ app/src/main/java/com/example/firebaseemailaccount/LoginActivity.java
@@ -67,55 +67,17 @@ public class LoginActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     UserAccount user = new UserAccount(et_email.getText().toString(), et_pwd.getText().toString());
                     call = Retrofit_client.getUserApiService().user_login(user);
-                    // Call<UserAccount> cookie = Retrofit_client.getUserApiService().getCookie(user);
-
-                    // HTTP 요청 보내기
-                    // Request request = new Request.Builder()
-                    //         .url(BASE_URL) // 요청할 URL 설정
-                    //         .build();
 
                     call.enqueue(new Callback<UserAccount>() {
                         //콜백 받는 부분
                         @Override
                         public void onResponse(Call<UserAccount> call, Response<UserAccount> response) {
                             if(response.isSuccessful()){
-                                // Response res = client.newCall(request).execute();
-                                // UserAccount result = response.body(); // 애초에 user/login/의 response에 token 값밖에 없으므로 result.getEmail() 해봤자 null만 나옴. user.getEmail()이 맞음.
-                                // id = user.getId(); // 계속 0만 나옴
-                                // HashSet<String> cookies = new Cookie_jar().loadForRequest(response.raw().request().url());
-                                // List<Cookie> cookies = new Cookie_jar().loadForRequest(HttpUrl.parse(BASE_URL + "user/login/"));
-
-                                // for (Cookie cookie : cookies) {
-                                //     if (cookie.name().equals("jwt")) {
-                                //         String token = cookie.value();
-                                //         new Cookie_jar().saveFromResponse(HttpUrl.parse(BASE_URL + "user/login/"), cookies);
-                                //     }
-                                // }
-
-                                // Store the cookies in the CookieJar for subsequent requests
-                                // new Cookie_jar().saveFromResponse(HttpUrl.parse(BASE_URL + "user/login/"), cookies);
-                                // cookies.isEmpty() // true 나옴(cookie값 제대로 못 받아옴)
-
-                                // 서버에서 설정해놓은 AUTH_HEADER_NAME으로 설정해서 header 값 받아오기
-                                // request.META.get('HTTP_AUTHORIZATION');
-
-                                // ---------------- SharedPreferences 저장 방식 ----------------
-                                // user_cookie란 이름의 파일에 SharedPreferences 저장소(공유 환경설정 파일) 생성?
-                                // SharedPreferences sharedPref = getSharedPreferences("user_cookie", Context.MODE_PRIVATE);
-                                // 초기화 느낌?
-                                // Set<String> cookies = sharedPref.getStringSet("cookie", new HashSet<String>());
-                                // SharedPreferences.Editor editor = sharedPref.edit();
-                                // 해당 값 지정하여 전달
-                                // 첫번째 이름의 변수에 두번째 인수 값을 저장하여 공유 환경설정 파일에 저장 -> putInt 혹은 putString 사용
-                                // 서버로부터 cookies 받아와서 저장해야 함
-                                // editor.putStringSet("cookie", cookies);
-                                // editor.apply(); // 실제로 저장
-
                                 // ---------------- 로그인 후 받아온 데이터 확인해보기 ----------------
                                 // Log() 사용하여 test 삼아 확인
-                                Log.v("login-test", "jwtToken:" + response.body().getJwt());
+                                // Log.v("login-test", "jwtToken:" + response.body().getJwt());
 
-                                // ---------------- SharedPreferences에 저장하기 ----------------
+                                // ---------------- SharedPreferences에 cookie 저장하기 ----------------
                                 // 공유 환경설정 파일 이름 지정
                                 sharedPref = getSharedPreferences("user_cookie", Context.MODE_PRIVATE);
                                 SharedPreferences.Editor editor = sharedPref.edit();
@@ -123,12 +85,10 @@ public class LoginActivity extends AppCompatActivity {
                                 editor.apply();
 
                                 // ---------------- (test)SharedPreferences에 저장한 것 읽어와보기 ----------------
-                                String test_cookie = sharedPref.getString("cookie", "abcd");
+                                // String test_cookie = sharedPref.getString("cookie", "abcd");
 
                                 Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_LONG).show();
-                                String email = user.getEmail(); // 위에서 바로 지정했으므로 제대로 나옴
                                 Intent intent = new Intent(LoginActivity.this, PageActivity.class);
-                                intent.putExtra("email", email); // 로그인된 사용자의 email 전달(id는 계속 0만 나와서 사용 불가능)
                                 startActivity(intent); // 화면 전환
                             }else{
                                 Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_LONG).show();
@@ -140,19 +100,6 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     });
                 }
-
-//                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            Intent intent = new Intent(LoginActivity.this, PageActivity.class);
-//                            startActivity(intent);
-//                            finish();
-//                        } else {
-//                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
             });
 
             Button btn_register = findViewById(R.id.btn_register);
--- app/src/main/java/com/example/firebaseemailaccount/MyPageActivity.java
+++ app/src/main/java/com/example/firebaseemailaccount/MyPageActivity.java
@@ -26,7 +26,7 @@ public class MyPageActivity extends Fragment {
     private View view;
     private String TAG = "MyPageFragment";
     Call<UserAccount> call;
-    Button update_button;
+    Button update_button, logout_button;
     TextView nickname_view, baby_birthday_view, baby_gender_view;
 
     public static MyPageActivity newInstance() {
@@ -41,16 +41,11 @@ public class MyPageActivity extends Fragment {
         view = inflater.inflate(R.layout.activity_mypage,container,false);
 
         update_button = view.findViewById(R.id.update_button);
+        logout_button = view.findViewById(R.id.logout_button);
         nickname_view = view.findViewById(R.id.nickname_view);
         baby_birthday_view = view.findViewById(R.id.baby_birthday_view);
         baby_gender_view = view.findViewById(R.id.baby_gender_view);
 
-        // 서버에서는 user/view/{id}로 번호 지정해서 user 계정 정보 가져오는 것과
-        // 로그인되었을 때에만 접근되는 것도 다 구현됐는데 막상 클라이언트에서 호출하면
-        // 다른 activity로 이동하면 로그인이 다시 풀려서 로그인을 했음에도 불구하고 다른 카테고리에 접근이 제대로 되지 않는 상황
-        // Intent intent=getActivity().getIntent();
-        // id = intent.getIntExtra("id", 1);
-        // String c_email = intent.getStringExtra("email");
         // SharedPreferences sharedPref = getSharedPreferences("user_cookie", Context.MODE_PRIVATE);
         String login_cookie = LoginActivity.sharedPref.getString("cookie", "abcd");
         call = Retrofit_client.getUserApiService().user_view(login_cookie);
@@ -75,6 +70,39 @@ public class MyPageActivity extends Fragment {
             @Override
             public void onClick(View v) {
                 ((PageActivity)getActivity()).replaceFragment(MyPageUpdateActivity.newInstance());
+            }
+        });
+
+        // '로그아웃' 버튼 클릭 시 cookie 삭제
+        // 로그아웃 된 화면의 마이페이지로 전환
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // token 값 전달하여 서버에 로그인된 상태였음을 알림
+                call = Retrofit_client.getUserApiService().user_logout(login_cookie);
+                call.enqueue(new Callback<UserAccount>() {
+                    @Override
+                    public void onResponse(Call<UserAccount> call, Response<UserAccount> response) {
+                        if (response.isSuccessful()) {
+                            // 서버 상에서 로그아웃 완료 + 안드로이드 상에서 저장해놨던 cookie 삭제하기
+                            SharedPreferences.Editor editor = LoginActivity.sharedPref.edit();
+                            editor.remove("cookie");
+                            editor.apply();
+                            Toast.makeText(getContext(), "로그아웃 성공", Toast.LENGTH_LONG).show();
+                            // 1) 로그아웃 후 로그인 전의 마이페이지로 리디렉션할 경우
+                            // ((PageActivity)getActivity()).replaceFragment(MyPageActivity.newInstance());
+                            // 2) 로그아웃 후 처음 로그인 화면으로 리디렉션할 경우
+                            Intent intent = new Intent(getContext(), LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getContext(), "로그아웃 실행되지 않음", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<UserAccount> call, Throwable t) {
+                        Toast.makeText(getContext(), "로그아웃 실패", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
